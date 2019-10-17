3e62605d329f7b9e9dd973b9f3638aed4cfd6194
Nicholas Allen Snider <snidern4@egr.msu.edu>
2019-10-16 22:48:27 -0400

    proj04 complete

--- Project4/CircularQueue.py
+++ Project4/CircularQueue.py
@@ -47,39 +47,60 @@ class CircularQueue:
     # -----------MODIFY BELOW--------------
 
     def is_empty(self):
+        """
+        checks to see if queue is empty
+        :return: [bool] True if empty, false otherwise
+        """
         if self.head is 0 and self.tail is 0:
             return True
         else:
             return False
 
     def __len__(self):
+        """
+        gets length of queue
+        :return: [int] returns the size of queue
+        """
         return self.size
 
     def get_total(self):
-        sum_total = 0
-        node = self.head
-        while node is not self.tail:
-            sum_total += self.data[node]
-            node+=1
-        return sum_total
+        """
+        gets the sum of every element in the queue
+        :return: [int] sum of every element
+        """
+        return self.total
 
     def head_element(self):
+        """
+        gets the head element of the queue
+        :return: [int] the value of head
+        """
         return self.data[self.head]
 
     def tail_element(self):
+        """
+        gets the value of the tail
+        :return: [int] value of tail
+        """
         if self.tail == 0:
             return self.data[self.tail]
         else:
             return self.data[self.tail - 1]
 
     def enqueue(self, val):
-        # if val is None:
-        #     return None
+        """
+        adds an element to the back of the queue
+        :param val: [int] value to add to queue
+        :return: None
+        """
+        if val is None:
+            return None
+        # add to total
+        self.total += val
         # gets index of next open index
         emptyIndex = (self.head + self.size) % self.capacity
         self.data[emptyIndex] = val
-        if val is not None:
-            self.size += 1
+        self.size += 1
         self.tail = emptyIndex + 1
         # if queue needs more size
         if self.size == self.capacity:
@@ -87,10 +108,17 @@ class CircularQueue:
         return None
 
     def dequeue(self):
+        """
+        removes the first element in the queue and returns it
+        :return: [int] head of the queue
+        """
         # if list is empty
         if self.size is 0:
             return None
+        # functions as pop method, so return the front index
         return_val = self.data[self.head]
+        # subtract from total
+        self.total -= return_val
         self.data[self.head] = None
         # if only one element set both head and tail to 0
         if self.size is 1:
@@ -103,17 +131,23 @@ class CircularQueue:
             self.shrink()
         return return_val
 
-
     def grow(self):
+        """
+        if capacity is reached, double the size of the queue
+        :return: No return, queue is doubled in capacity
+        """
         # gets the index of start of new list
         next_index = self.capacity
         self.capacity = self.capacity * 2
         while next_index != self.capacity:
             self.data.append(None)
-            next_index+=1
-
+            next_index += 1
 
     def shrink(self):
+        """
+        if size is 1/4 or less of capcity shrink the size by half
+        :return:no return, shrinks the size in half but no less than 4
+        """
         # calculates the number of "None"s to delete
         delete_num = self.head - 0
         # capacity never goes lower than 2
@@ -125,12 +159,52 @@ class CircularQueue:
         if delete_num is not 0:
             while delete_num != 0:
                 self.data.pop(delete_num - 1)
-                delete_num-=1
-        self.data = self.data[0:self.capacity]
+                delete_num -= 1
+        # if capacity isn't full
+        if len(self.data) < self.capacity:
+            while len(self.data) < self.capacity:
+                self.data.append(None)
+                if len(self.data) == self.capacity:
+                    break
+        # if size of data has more elements than capacity
+        elif len(self.data) > self.capacity:
+            while len(self.data) > self.capacity:
+                self.data.remove(None)
+                if len(self.data) == self.capacity:
+                    break
         self.head = 0
         self.tail = self.data.index(None)
 
 
+def threshold_sum(nums, threshold):  # [1,2,3,None,None]
+    """
+    find the sequence of consecutive numbers with the highest possible
+    sum that is less than or equal to the threshold
+    :param nums: [list] 
+    :param threshold:
+    :return: [tuple] (sum of elements in sequence, length of sequence)
+    """
+    if threshold < 0:
+        return 0, 0
+    queue = CircularQueue()
+    largest_sum = 0
+    largest_seq = 0
+    for i in nums:
+        if queue.get_total() <= threshold:
+            queue.enqueue(i)
+        while queue.get_total() > threshold:
+            queue.dequeue()
+        if largest_sum < queue.get_total() <= threshold:
+            largest_sum = queue.get_total()
+            largest_seq = len(queue)
+        if largest_sum == queue.get_total():
+            if largest_seq < len(queue):
+                largest_seq = len(queue)
 
-def threshold_sum(nums, threshold):
-    pass
+    if largest_sum == 0:
+        if sum(nums) == 0:
+            return 0, len(nums)
+        else:
+            return 0, len(queue)
+    else:
+        return largest_sum, largest_seq
--- Project4/mimir_testcases.py
+++ Project4/mimir_testcases.py
@@ -18,9 +18,10 @@ class TestProject1(unittest.TestCase):
         assert queue.get_total() == 10
         assert queue.head_element() == 1
         assert queue.tail_element() == 4
+
     #
     def test_enqueue(self):
-        # if item to add is None don't increase size
+        # if item to add is None don't increase size`
         queue = CircularQueue()
         queue.enqueue(None)
         assert queue.data == [None, None, None, None]
@@ -33,7 +34,7 @@ class TestProject1(unittest.TestCase):
         queue.enqueue(10)
         queue.enqueue(20)
         queue.enqueue(30)
-        assert queue.data == [10,20,30,None]
+        assert queue.data == [10, 20, 30, None]
         assert queue.size == 3
         assert queue.head == 0
         assert queue.tail == 3
@@ -43,7 +44,7 @@ class TestProject1(unittest.TestCase):
         queue.enqueue(10)
         queue.enqueue(None)
         queue.enqueue(30)
-        assert queue.data == [10,30, None,None]
+        assert queue.data == [10, 30, None, None]
         assert queue.size == 2
         assert queue.head == 0
         assert queue.tail == 2
@@ -62,6 +63,7 @@ class TestProject1(unittest.TestCase):
         assert queue.tail == 5
         assert queue.capacity == 10
         #
+
     def test_dequeue(self):
         # mimir test copied
         queue = CircularQueue(6)
@@ -78,7 +80,7 @@ class TestProject1(unittest.TestCase):
         assert queue.capacity == 6
         assert queue.head == 1
         assert queue.tail == 5
-        #smaller test
+        # smaller test
         queue = CircularQueue(4)
         for i in range(0, 3):
             queue.enqueue(i * 5)
@@ -132,7 +134,7 @@ class TestProject1(unittest.TestCase):
         queue = CircularQueue(10)
         for i in range(1, 6):
             queue.enqueue(i)
-        assert queue.data == [1,2,3,4,5, None, None, None, None, None]
+        assert queue.data == [1, 2, 3, 4, 5, None, None, None, None, None]
         assert queue.size == 5
         assert queue.capacity == 10
         assert queue.head == 0
@@ -164,13 +166,13 @@ class TestProject1(unittest.TestCase):
         assert queue.size == 8
         assert queue.head == 0
         assert queue.tail == 8
+
     #
     def test_shrink(self):
         queue = CircularQueue()
 
         for i in range(0, 5):
             queue.enqueue(i)
-
         assert queue.data == [0, 1, 2, 3, 4, None, None, None]
         assert queue.size == 5
         assert queue.capacity == 8
@@ -191,12 +193,14 @@ class TestProject1(unittest.TestCase):
         assert queue.head == 0
         assert queue.tail == 0
         queue.dequeue()
-        queue.dequeue()
+        test = queue.dequeue()
+        assert test == None
         assert queue.data == [None, None, None, None, None, None, None]
         assert queue.size == 0
         assert queue.capacity == 7
         assert queue.head == 0
         assert queue.tail == 0
+        assert queue.total == 0
         # empty list that has the ability to shrink but shouldn't
         queue = CircularQueue(8)
         assert queue.data == [None, None, None, None, None, None, None, None]
@@ -216,42 +220,152 @@ class TestProject1(unittest.TestCase):
         queue = CircularQueue(9)
         for i in range(0, 3):
             queue.enqueue(i)
-        print(queue.data)
         assert queue.data == [0, 1, 2, None, None, None, None, None, None]
         assert queue.size == 3
         assert queue.capacity == 9
         assert queue.head == 0
         assert queue.tail == 3
         queue.dequeue()
-        assert queue.data == [1,2,None,None]
+        assert queue.data == [1, 2, None, None]
         assert queue.size == 2
         assert queue.capacity == 4
         assert queue.head == 0
         assert queue.tail == 2
-        # shrink from 9 to 4
+        assert queue.total == 3
+        # test #3 from piazza student
         queue = CircularQueue(5)
-        queue.enqueue(None)
-        queue.enqueue(None)
         queue.enqueue(5)
-
-        print(queue.data)
-
-        assert queue.data == [None, None, 5, None, None]
-        assert queue.size == 1
+        queue.enqueue(5)
+        queue.enqueue(2)
+        queue.dequeue()
+        queue.dequeue()
+        # [None. None, 5, None, None]
+        queue.dequeue()
+        assert queue.data == [None, None, None, None, None]
+        assert queue.size == 0
         assert queue.capacity == 5
+        assert queue.head == 0
+        assert queue.tail == 0
+        assert queue.total == 0
+        # test #1 from piazza student
+        queue = CircularQueue(13)
+        for i in range(0, 7):
+            queue.enqueue(i)
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        # [None, None, None, 3, 4, 5, 6, None, None, None, None, None, None]
+        assert queue.data == [None, None, None, 3, 4, 5, 6, None, None, None, None, None, None]
+        assert queue.size == 4
+        assert queue.capacity == 13
+        assert queue.head == 3
+        assert queue.tail == 7
+        assert queue.total == 18
+        queue.dequeue()
+        assert queue.data == [4, 5, 6, None, None, None]
+        assert queue.size == 3
+        assert queue.capacity == 6
+        assert queue.head == 0
+        assert queue.tail == 3
+        assert queue.total == 15
+        queue.dequeue()
+        assert queue.data == [None, 5, 6, None, None, None]
+        assert queue.size == 2
+        assert queue.capacity == 6
+        assert queue.head == 1
+        assert queue.tail == 3
+        assert queue.total == 11
+        queue.dequeue()
+        assert queue.data == [None, None, 6, None, None, None]
+        assert queue.size == 1
+        assert queue.capacity == 6
         assert queue.head == 2
         assert queue.tail == 3
-        # queue.dequeue()
-        # queue.data = [None, None, None, None, None]
-        # assert queue.size == 0
-        # assert queue.capacity == 5
-        # assert queue.head == 0
-        # assert queue.tail == 0
+        assert queue.total == 6
+        # one item in large list, dequeue and then shrink
+        queue = CircularQueue(8)
+        queue.enqueue(1)
+        assert queue.data == [1, None, None, None, None, None, None, None]
+        assert queue.size == 1
+        assert queue.capacity == 8
+        assert queue.head == 0
+        assert queue.tail == 1
+        test = queue.dequeue()
+        assert test == 1
+        assert queue.data == [None, None, None, None]
+        assert queue.size == 0
+        assert queue.capacity == 4
+        assert queue.head == 0
+        assert queue.tail == 0
+        assert queue.total == 0
+        # possibility of adding None's to end
+        queue = CircularQueue(10)
+        for i in range(0, 9):
+            queue.enqueue(i)
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
 
-    # def test_threshold_sum(self):
-    #     assert threshold_sum([1, 2, 3, 4], 4) == (4, 1)
-    #     assert threshold_sum([1, 3, 2, 4], 4) == (4, 2)
-    #     assert threshold_sum([1, 2, 3, 4], 8) == (7, 2)
+    def test_threshold_sum(self):
+        assert threshold_sum([1, 2, 3, 4], 4) == (4, 1)  # [4, 1]
+        assert threshold_sum([1, 3, 2, 4], 4) == (4, 2)  # [1, 3]
+        assert threshold_sum([1, 2, 3, 4], 8) == (7, 2)  # [3, 4]
+
+        #
+        # assert threshold_sum([1, 2, 3, 4], 4) == (4, 1)
+        # assert threshold_sum([1, 3, 2, 4], 4) == (4, 2)
+        # assert threshold_sum([1, 2, 3, 4], 8) == (7, 2)
+        # assert threshold_sum([2], 4) == (2, 1)
+        # print("Jeez this kid next to me smells like a cheese puff!")
+        # assert threshold_sum([1, 2, 7, 3], 4) == (3, 2)
+        # assert threshold_sum([1, 2, 2, 3], 6) == (5, 3)
+        # assert threshold_sum([4, 5, 6, 7], 3) == (0, 0)
+        # assert threshold_sum([2, 2, 4, 1, 1], 4) == (4, 2)
+        # assert threshold_sum([2, 3, 4, 5], 2) == (2, 1)
+        # assert threshold_sum([1, 2, 4, 3], 7) == (7, 3)
+        # assert threshold_sum([], 1) == (0, 0)
+        # assert threshold_sum([1, 2, 7, 3, 324, 54, 6, 7, 87, 8, 8, 9, 9, 9, 9, 99, 9, 9], 40) == (36, 4)
+        # assert threshold_sum([8, 7, 6, 5], 4) == (0, 0)
+        # assert threshold_sum([1, 4, 2, 4], 3) == (2, 1)
+        # assert threshold_sum([2, 2, 4, 1, 1], 4) == (4, 2)
+        # assert threshold_sum([1, 3, 2, 4], 4) == (4, 2)
+        # assert threshold_sum([1, 2, 3, 4], 8) == (7, 2)
+        # assert threshold_sum([2, 2, 4, 1, 1], 4) == (4, 2)
+        # assert threshold_sum([1, 2, 2, 3], 6) == (5, 3)
+        # assert threshold_sum([1, 2, 4, 3], -7) == (0, 0)
+        # assert threshold_sum([], 0) == (0, 0)
+        # assert threshold_sum([1, 2, 7, 3, 324, 54, 6, 7, 87, 8, 8, 9, 9, 9, 9, 99, 9, 9], 40) == (36, 4)
+        # assert threshold_sum([8, 7, 6, 5], 4) == (0, 0)
+        # assert threshold_sum([1], 4) == (1, 1)
+        # assert threshold_sum([8, 7, 6, 5, 8, 9, 5, 3, 6, 8, 6], 4) == (3, 1)
+        # assert threshold_sum([4, 1, 2, 1], 4) == (4, 3)
+        # assert threshold_sum([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0) == (0, 12)
+        # assert threshold_sum([1, 1, 2, 1, 1, 2], 4) == (4, 3)
+        # assert threshold_sum([1, 2, 2, 3, 4], 6) == (5, 3)
+        # assert threshold_sum([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2) == (0, 12)
+        # assert threshold_sum([1, 0, 2, 4], 3) == (3, 3)
+        # assert threshold_sum([2, 0, 0, 1, 0, 2, 4], 3) == (3, 5)
+        # assert threshold_sum([2, 0, 0, 0, 1, 0, 0, 2, 4], 3) == (3, 7)
+        #
+        #
+        # assert threshold_sum([4,3,2,1], 4) == (4, 1)
+        # assert threshold_sum([4,2,3,1], 4) == (4, 1)
+        # assert threshold_sum([1, 2, 3, 4], 8) == (7, 2)
+        # assert threshold_sum([2], 4) == (2, 1)
+        # assert threshold_sum([1, 2, 7, 3], 4) == (3, 2)
+        # assert threshold_sum([1, 2, 2, 3], 6) == (5, 3)
+        # assert threshold_sum([4, 5, 6, 7], 3) == (0, 0)
+        # assert threshold_sum([2, 2, 4, 1, 1], 4) == (4, 2)
+        # assert threshold_sum([2,3, 4,5], 2) == (2, 1)
+        # assert threshold_sum([1, 2, 4, 3], 7) == (7, 3)
+        # assert threshold_sum([], 1) == (0, 0)
+        # assert threshold_sum([1, 2, 7, 3, 324, 54, 6, 7, 87, 8, 8, 9, 9, 9, 9, 99, 9, 9], 40) == (36, 4)
+        # assert threshold_sum([8, 7, 6, 5], 4) == (0, 0)
+        # assert threshold_sum([1, 4, 2, 4], 3) == (2, 1)
 
 
 if __name__ == "__main__":
